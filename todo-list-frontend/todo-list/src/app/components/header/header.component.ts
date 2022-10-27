c85bbeec6676f8b20924ae0b33286730e06ef7ea
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-10-26 22:41:25 -0300

    feature: added a clock

--- todo-list-frontend/todo-list/src/app/components/header/header.component.ts
+++ todo-list-frontend/todo-list/src/app/components/header/header.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription, timer } from "rxjs";
+import { map, share } from "rxjs/operators";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  time = new Date();
+  rxTime = new Date();
+  intervalId: string | number | NodeJS.Timer | undefined;
+  subscription: Subscription | undefined;
 
-  constructor() { }
+  ngOnInit() {
+    // Using Basic Interval
+    this.intervalId = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
 
-  ngOnInit(): void {
+    // Using RxJS Timer
+    this.subscription = timer(0, 1000)
+      .pipe(
+        map(() => new Date()),
+        share()
+      )
+      .subscribe(time => {
+        this.rxTime = time;
+      });
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 }
