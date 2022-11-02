7743ff8fec55a4200f397c2fd27e96e60104bd5d
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-11-02 11:54:45 -0300

    update page matching design applied

--- todo-list-frontend/todo-list/src/app/app-routing.module.ts
+++ todo-list-frontend/todo-list/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FinishedComponent } from "./components/finished/finished.component";
 import { ReadAllComponent } from "./components/read-all/read-all.component";
+import { UpdateComponent } from "./components/update/update.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: "finished",
     component: FinishedComponent,
+  },
+  {
+    path: "update/:id",
+    component: UpdateComponent,
   },
 ];
 
--- todo-list-frontend/todo-list/src/app/components/update/update.component.ts
+++ todo-list-frontend/todo-list/src/app/components/update/update.component.ts
@@ -1,20 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Todo } from "src/app/models/todo";
+import { TodoService } from "src/app/services/todo.service";
 
 @Component({
-  selector: 'app-update',
-  template: `
-    <p>
-      update works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: "app-update",
+  templateUrl: "./update.component.html",
+  styleUrls: ["./update.component.css"],
 })
 export class UpdateComponent implements OnInit {
+  todo: Todo = {
+    title: "",
+    description: "",
+    dateToFinishTask: "",
+    finished: false,
+  };
 
-  constructor() { }
+  constructor(private service: TodoService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  create() {
+    this.formatDate();
+    this.service.create(this.todo).subscribe(
+      (answer) => {
+        this.service.message("New task added successfully");
+        this.clear();
+      },
+      (err) => {
+        this.service.message("Failed to create new task");
+        this.clear();
+      }
+    );
+    this.navigateToHomePage();
   }
 
+  clear() {
+    this.todo.description = "";
+    this.todo.title = "";
+    this.todo.dateToFinishTask.value = "";
+  }
+
+  formatDate(): void {
+    let date = new Date(this.todo.dateToFinishTask);
+    this.todo.dateToFinishTask = `${date.getDate()}/${
+      date.getMonth() + 1
+    }/${date.getFullYear()}`;
+  }
+
+  navigateToHomePage(): void {
+    this.router.navigate([""]);
+  }
 }
