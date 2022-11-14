fd5bc26abd3e59b0a8c5ac1177c6d7ae3d30522e
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-11-14 12:54:27 -0300

    corrected some bugs on mobile

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from "@angular/common";
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
 
@@ -11,5 +13,4 @@ export class FooterComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
